9ede1cf9d87954a6f3e9c3ad0e048a1e16486969
Pranesh <pran18me063@rmkcet.ac.in>
2024-08-08 14:40:14 +0530

    cors removal

--- server.js
+++ server.js
@@ -12,17 +12,6 @@ const cors = require('cors');
 //middleware
 app.use(express.json())
 
-// Allow requests from any origin
-app.use(cors());
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
-
 //routes
 app.use('/api/user', authRoutes);
 app.use('/api/slot', slotRoutes);
